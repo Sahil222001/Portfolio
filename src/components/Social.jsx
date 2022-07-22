6dab174c5cea72dc4805bdd2c32599fee4e5faaf
Sahil222001 <chorghesahil16@gmail.com>
2022-07-22 23:44:01 +0530

    my commit

--- src/components/Social.jsx
+++ src/components/Social.jsx
@@ -56,7 +56,7 @@ const Social = () => {
           {links.map(({id,child,href,style})=>
           (
           <li key={id} 
-          className={"flex justify-between items-center w-40 h-14 px-4  ml-[-100px] hover:ml-[-10px]  hover:rounded-md duration-500"+" "+style}
+          className={"flex justify-between items-center w-40 h-14 px-4  ml-[-100px] hover:ml-[-10px]  hover:rounded-md duration-500 "+style}
           >
              <a
               href={href} 
